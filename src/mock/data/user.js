52054cdd660e2e66519698e65406cd91c1a78224
weida <weida@ifm360.cn>
2017-11-06 17:56:04 +0800

    vue-element-admin

---
+++ src/mock/data/user.js
@@ -0,0 +1,25 @@
+import Mock from 'mockjs';
+const LoginUsers = [
+  {
+    id: 1,
+    username: 'admin',
+    password: '123456',
+    avatar: 'https://avatars3.githubusercontent.com/u/23181508?s=400&u=905c9b5654aa2bf97f0ecc461b60f3c0d27216b8&v=4',
+    name: '45°微笑看世界'
+  }
+];
+
+const Users = [];
+
+for (let i = 0; i < 86; i++) {
+  Users.push(Mock.mock({
+    id: Mock.Random.guid(),
+    name: Mock.Random.cname(),
+    addr: Mock.mock('@county(true)'),
+    'age|18-60': 1,
+    birth: Mock.Random.date(),
+    sex: Mock.Random.integer(0, 1)
+  }));
+}
+
+export { LoginUsers, Users };
---
+++ src/views/nav2/Page4.vue
@@ -0,0 +1,41 @@
+<template >
+    <div class="activePublic " >
+      <!-- 步骤组件 -->
+      <el-steps :space="200" :active="step" class="step" style="margin:20px 0;">
+        <el-step title="活动信息" description=""></el-step>
+        <el-step title="报名签到" description=""></el-step>
+        <el-step title="分享设置" description=""></el-step>
+        <el-step title="个性设置" description=""></el-step>
+      </el-steps>
+
+        <!-- 视图 -->
+      <router-view class="view"></router-view>
+
+      <div class="but-group" >
+        <el-button >预览</el-button>
+        <el-button >上一步</el-button>
+        <el-button  type="primary" @click="next">下一步</el-button>
+        <el-button  type="primary">发布活动</el-button>
+<!--         <router-link :to="{name:'bb'}">bb</router-link>
+        <router-link to="/activePublic/aa">Go to Foo</router-link> -->
+      </div>
+
+      <!-- <a v-link="a">aa</a> -->
+    </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        step: 0
+      };
+    },
+
+    methods: {
+      next() {
+        this.step ++;
+      }
+    }
+  }
+</script>
